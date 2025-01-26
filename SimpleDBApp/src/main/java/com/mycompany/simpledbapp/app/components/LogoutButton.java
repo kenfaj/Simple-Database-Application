5d86317b159b495795ca3b602698833bdbeb2646
kenfaj <149665960+kenfaj@users.noreply.github.com>
2025-01-26 19:07:12 +0800

    Gui drafts

---
+++ SimpleDBApp/src/main/java/com/mycompany/simpledbapp/app/components/LogoutButton.java
@@ -0,0 +1,10 @@
+package com.mycompany.simpledbapp.app.components;
+
+import javax.swing.JButton;
+
+public class LogoutButton extends JButton {
+    public LogoutButton() {
+        setText("Logout");
+        setPreferredSize(new java.awt.Dimension(100, 30));
+    }
+}
