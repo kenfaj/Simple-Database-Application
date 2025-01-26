d7e0a41605bec0a15d4c39ca0c87e261b237d6af
kenfaj <149665960+kenfaj@users.noreply.github.com>
2025-01-26 13:11:33 +0800

    Initial Structure w/ initialization file

---
+++ SimpleDBApp/src/main/java/com/mycompany/simpledbapp/init.java
@@ -0,0 +1,8 @@
+package com.mycompany.simpledbapp;
+
+public class init {
+    public static void main(String[] args) {
+        //Dito tayo magiinitialize ng Application(open login frame)
+        
+    }
+}
---
+++ SimpleDBApp/src/main/java/com/mycompany/simpledbapp/view/Guest.java
@@ -0,0 +1,5 @@
+package com.mycompany.simpledbapp.view;
+
+public class Guest {
+    
+}
