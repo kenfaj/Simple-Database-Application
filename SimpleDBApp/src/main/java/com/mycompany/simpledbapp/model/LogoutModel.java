0836ed585928edd1f95713286ddd73fcff93aaa5
kenDRoger <kengabriell.fajardo.cics@ust.edu.ph>
2025-01-26 19:24:35 +0800

    init/admin/guest/login/logoutbuttoncontroller

--- SimpleDBApp/src/main/java/com/mycompany/simpledbapp/model/LogoutModel.java
+++
@@ -1,5 +0,0 @@
-package com.mycompany.simpledbapp.model;
-
-public class LogoutModel {
-    
-}
--- SimpleDBApp/src/main/java/com/mycompany/simpledbapp/service/LogoutButtonController.java
+++ SimpleDBApp/src/main/java/com/mycompany/simpledbapp/service/LogoutButtonController.java
@@ -3,29 +3,14 @@ package com.mycompany.simpledbapp.service;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-
+import com.mycompany.simpledbapp.init;
 import com.mycompany.simpledbapp.app.Login;
-import com.mycompany.simpledbapp.model.LogoutModel;
 
 public class LogoutButtonController implements ActionListener {
-
-    private LogoutModel model;
-    private JFrame view;
-
-    public LogoutButtonController(LogoutModel model, JFrame view) {
-        this.model = model;
-        this.view = view;
-    }
-
-    // Method to attach the listener to a button
-    // public void attachListener(LogoutButton logoutButton) {
-
-    // }
-
-    
     public void actionPerformed(ActionEvent e) {
-        model.logout();
-        view.setNewContent(new Login().launch());
+        init.username = null;// set username to null
+        init.frame.setContentPane(new Login().launch());// switch to login
+        init.frame.revalidate();
+        init.frame.repaint();
     }
 }
