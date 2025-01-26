8018863433efecc42e431da087f45c87f5cdbe83
kenfaj <149665960+kenfaj@users.noreply.github.com>
2025-01-26 19:03:40 +0800

    LogoutButtonController.java draft

---
+++ SimpleDBApp/src/main/java/com/mycompany/simpledbapp/service/LogoutButtonController.java
@@ -0,0 +1,32 @@
+package com.mycompany.simpledbapp.service;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import com.mycompany.simpledbapp.app.Login;
+import com.mycompany.simpledbapp.model.LogoutModel;
+
+public class LogoutButtonController implements ActionListener {
+
+    private LogoutModel model;
+    private JFrame view;
+
+    public LogoutButtonController(LogoutModel model, JFrame view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    // Metho
+    / public void attachListener(LogoutButton logoutButto
+
+    ublic void actionPerformed(ActionEvent e) {
+        /*
+       
+
+     * model.logout();
+         * view.setNewContent(new Login().launch());
+         */
+    }
+}
