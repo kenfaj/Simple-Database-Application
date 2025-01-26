f8daa91f73dd8298eb586002e5fc5df8ffcfb5da
kenfaj <149665960+kenfaj@users.noreply.github.com>
2025-01-26 11:21:08 +0800

    Initial Commit SimpleDBApp draft

---
+++ SimpleDBApp/src/main/java/com/mycompany/simpledbapp/Gui.java
@@ -0,0 +1,57 @@
+package com.mycompany.simpledbapp;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Gui {
+
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel userLabel;
+    private JLabel passwordLabel;
+    private JTextField userTextField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+
+    // setup connection object
+
+    public Gui() {
+        frame = new JFrame("Login");
+        panel = new JPanel();
+        userLabel = new JLabel("Enter Username");
+        passwordLabel = new JLabel("Enter Password");
+        userTextField = new JTextField();
+        passwordField = new JPasswordField();
+        loginButton = new JButton("Login");
+    }
+
+    public void launch() {
+
+        panel.setLayout(new GridLayout(2, 2));
+        panel.add(userLabel);
+        panel.add(userTextField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.add(loginButton, BorderLayout.SOUTH);
+        frame.setVisible(true);
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        loginButton.addActionListener(new ActionListener() {
+
+        });
+    }
+
+    public static void main(String[] args) {
+        Gui gui = new Gui();
+        gui.launch();
+    }
+
+}
---
+++ SimpleDBApp/src/main/java/com/mycompany/simpledbapp/JDBCStartup.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.simpledbapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author ken
+ */
+public class JDBCStartup {
+    private class User {
+        String username;
+        String password;
+        String accessRole;
+
+        public User(String username, String password, String accessRole) {
+            this.username = username;
+            this.password = password;
+            this.accessRole = accessRole;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getAccessRole() {
+            return accessRole;
+        }
+
+        public void setAccessRole(String accessRole) {
+            this.accessRole = accessRole;
+        }
+    }
+
+    private Connection conn;
+
+    public JDBCStartup(String username, String password, String database) {
+        String connStr = "jdbc:sqlite:resources/" + database;
+        try {
+            conn = DriverManager.getConnection(connStr);
+            System.out.println("Successful connection");
+        } catch (SQLException e) {
+            System.err.println("Failed to create connection");
+            System.err.println(e.toString());
+        }
+    }
+
+    public ResultSet getAll() throws SQLException {
+        Statement stmt = conn.createStatement();
+        return stmt.executeQuery("select * from Users");
+    }
+
+    public User getUser(String username, String password, String accessRole) throws SQLException {
+        Statement stmt = conn.createStatement();
+        String sqlStr = "select * from users a "
+                + "where a.username=" + username + " "
+                + "& a.password=" + password
+                + "& a.user_role=" + accessRole;
+        ResultSet rs = stmt.executeQuery(sqlStr);
+        if (rs.next())
+            return new User(rs.getString("username"), rs.getString("password"),
+                    rs.getString("access_role"));
+        else
+            return null;
+    }
+
+    public User getUserByUserName(String username) throws SQLException {
+        Statement stmt = conn.createStatement();
+        String sqlStr = "select * from users a "
+                + "where a.username=" + username;
+        ResultSet rs = stmt.executeQuery(sqlStr);
+        if (rs.next())
+            return new User(rs.getString("username"), rs.getString("password"),
+                    rs.getString("access_role"));
+        else
+            return null;
+    }
+
+    public static void main(String[] args) {
+        JDBCStartup jdbc = new JDBCStartup("root", "root", "AccountsDB.db");
+        try {
+            ResultSet rs = jdbc.getAll();
+            while (rs.next()) {
+                System.out.println(rs.getString("username") + rs.getString("password") + rs.getString("user_role"));
+            }
+        } catch (SQLException e) {
+            System.err.println(e.toString());
+        }
+    }
+}
