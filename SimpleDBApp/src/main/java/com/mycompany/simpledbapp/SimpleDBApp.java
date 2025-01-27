ed324ff6f053ce1f37c23b73bc584cddf61b374c
kenDRoger <kengabriell.fajardo.cics@ust.edu.ph>
2025-01-27 15:19:22 +0800

    Fixes to start project

--- SimpleDBApp/src/main/java/com/mycompany/simpledbapp/SimpleDBApp.java
+++ SimpleDBApp/src/main/java/com/mycompany/simpledbapp/SimpleDBApp.java
@@ -6,13 +6,14 @@ import javax.swing.JFrame;
 public class SimpleDBApp {
 
     // stores current logged in user
-    public static String username;
+    public static String username = null;
     public static JFrame frame = new JFrame();
 
     public static void main(String[] args) {
         // Dito tayo magiinitialize ng Application(open login frame)
         frame.add(new Login().launch());
         frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 }
