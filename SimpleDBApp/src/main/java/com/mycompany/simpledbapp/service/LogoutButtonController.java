bd78fe399389e2207ce279a78304ef28b6041202
kenDRoger <kengabriell.fajardo.cics@ust.edu.ph>
2025-01-27 15:13:34 +0800

    Fixes to run whole project

--- SimpleDBApp/src/main/java/com/mycompany/simpledbapp/service/LogoutButtonController.java
+++ SimpleDBApp/src/main/java/com/mycompany/simpledbapp/service/LogoutButtonController.java
@@ -3,14 +3,14 @@ package com.mycompany.simpledbapp.service;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import com.mycompany.simpledbapp.init;
+import com.mycompany.simpledbapp.SimpleDBApp;
 import com.mycompany.simpledbapp.app.Login;
 
 public class LogoutButtonController implements ActionListener {
     public void actionPerformed(ActionEvent e) {
-        init.username = null;// set username to null
-        init.frame.setContentPane(new Login().launch());// switch to login
-        init.frame.revalidate();
-        init.frame.repaint();
+        SimpleDBApp.username = null;// set username to null
+        SimpleDBApp.frame.setContentPane(new Login().launch());// switch to login
+        SimpleDBApp.frame.revalidate();
+        SimpleDBApp.frame.repaint();
     }
 }
