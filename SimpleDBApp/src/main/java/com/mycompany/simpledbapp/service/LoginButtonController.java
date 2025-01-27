c12a1c8ccd0612fede690a220f8a65d1b0ef3913
kenfaj <149665960+kenfaj@users.noreply.github.com>
2025-01-27 19:38:25 +0800

    Implemented LoginButtonController

--- SimpleDBApp/src/main/java/com/mycompany/simpledbapp/service/LoginButtonController.java
+++ SimpleDBApp/src/main/java/com/mycompany/simpledbapp/service/LoginButtonController.java
@@ -2,13 +2,17 @@ package com.mycompany.simpledbapp.service;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 import com.mycompany.simpledbapp.SimpleDBApp;
 import com.mycompany.simpledbapp.app.Guest;
+import com.mycompany.simpledbapp.model.JDBCStartup;
 import com.mycompany.simpledbapp.app.Admin;
 
 public class LoginButtonController implements ActionListener {
@@ -21,6 +25,13 @@ public class LoginButtonController implements ActionListener {
         this.frame = frame;
         this.userTextField = userTextField;
         this.passwordField = passwordField;
+
+        jdbc = new JDBCStartup("AccountsDB.db");
+        try {
+            rs = jdbc.getAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -28,20 +39,51 @@ public class LoginButtonController implements ActionListener {
 
         String username = userTextField.getText();
         char[] password = passwordField.getPassword();
-        if (username.equals("admin") && password.equals("admin")) {
-            frame.setContentPane(new Admin(frame).launch()); // switch to admin
+
+        if (!isUser(username, password)) {
+            JOptionPane.showMessageDialog(null, "Invalid Username or Password");
         } else {
-            frame.setContentPane(new Guest(frame).launch()); // switch to guest
+            SimpleDBApp.username = username;
+            if (isAdmin(username, password)) {
+                frame.setContentPane(new Admin(frame).launch());
+            } else {
+                frame.setContentPane(new Guest(frame).launch());
+            }
         }
-        frame.revalidate();
+         
+
         frame.repaint();
     }
 
-    public boolean isUser(String username, char[] password) {
+    private JDBCStartup jdbc;
+    private ResultSet rs;
 
+    public boolean isUser(String username, char[] password) {
+        try {
+            while (rs.next()) {
+                if (rs.getString("username").equals(username) &&
+                        rs.getString("password").equals(new String(password))) {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public boolean isAdmin(String username, char[] password) {
-
+        try {
+            while (rs.next()) {
+                if (rs.getString("username").equals(username) &&
+                        rs.getString("password").equals(new String(password)) &&
+                        rs.getString("access_role").equals("admin")) {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
