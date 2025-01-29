bbdcadb78035c714d7467b5ff02ace733cd83a01
kenfaj <149665960+kenfaj@users.noreply.github.com>
2025-01-30 06:54:52 +0800

    resize issues in-progress

--- SimpleDBApp/src/main/java/com/mycompany/simpledbapp/SimpleDBApp.java
+++ SimpleDBApp/src/main/java/com/mycompany/simpledbapp/SimpleDBApp.java
@@ -1,6 +1,9 @@
 package com.mycompany.simpledbapp;
 
 import com.mycompany.simpledbapp.app.Login;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import javax.swing.JFrame;
 
 public class SimpleDBApp {
@@ -11,9 +14,19 @@ public class SimpleDBApp {
     public static void main(String[] args) {
         // Dito tayo magiinitialize ng Application(open login frame)
         JFrame frame = new JFrame();
+        
         frame.add(new Login(frame).launch());
-        frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        frame.setSize(400, 200);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().setBackground(new Color(40, 40, 40));
+        
         frame.setVisible(true);
     }
 }
